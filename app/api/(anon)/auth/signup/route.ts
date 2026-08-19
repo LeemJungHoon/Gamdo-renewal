c520e39da41887178a874ce074a15175351d6843
Leem Jung Hoon <xjqhdjq@naver.com>
2026-08-19 13:53:40 +0900

    fix : 회원가입 오류 수정 server.ts파일 추가에 따른 회원가입 코드 수정

--- app/api/(anon)/auth/signup/route.ts
+++ app/api/(anon)/auth/signup/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import { supabase } from "@/utils/supabase/client";
+import { supabaseAdmin } from "@/utils/supabase/server";
 import { SbUserRepository } from "@/backend/infrastructure/repositories/SbUserRepository";
 import { CreateSignupUseCase } from "@/backend/application/signup/usecases/CreateSignupUsecase";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    const userRepository = new SbUserRepository(supabase);
+    const userRepository = new SbUserRepository(supabaseAdmin);
     const signupUseCase = new CreateSignupUseCase(userRepository);
 
     const result = await signupUseCase.execute(body);
