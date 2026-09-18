222af1c62ea90fd2c580226dde8b4f4948041255
Leem Jung Hoon <xjqhdjq@naver.com>
2026-09-18 22:13:47 +0900

    chore : copyright 문구 수정

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full h-11 bg-gray-600 flex items-center justify-center">
       <div className="w-full h-full text-white flex justify-center items-center text-[12px]">
-        Gamdo @ 2025. All rights reserved.
+        Gamdo @ 2026. All rights reserved.
       </div>
     </footer>
   );
